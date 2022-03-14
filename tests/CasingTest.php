bba2fa40251783b23b36caa359fc8c178ccc523f
Rob Van Keilegom <rvk@organi.be>
2022-03-14 15:39:04 +0100

    :art: Format

--- tests/CasingTest.php
+++ tests/CasingTest.php
@@ -11,7 +11,7 @@ it('can convert camel to spaces', function () {
     $value = 'canConvertCamelToSpaces';
 
     $converter = new Casing(CaseStyle::CAMEL, CaseStyle::SPACES);
-    $out = $converter->convert($value);
+    $out       = $converter->convert($value);
 
     expect($out)->toBe('can convert camel to spaces');
 });
@@ -20,7 +20,7 @@ it('can convert kebab to camel', function () {
     $value = 'can-convert-camel-to-spaces';
 
     $converter = new Casing(CaseStyle::KEBAB, CaseStyle::CAMEL);
-    $out = $converter->convert($value);
+    $out       = $converter->convert($value);
 
     expect($out)->toBe('canConvertCamelToSpaces');
 });
